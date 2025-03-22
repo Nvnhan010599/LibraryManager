bf31cc3894d9e5a9b06e004fda2c06ef180476fd
Nhan McB <nvnhan@macbook>
2025-03-23 02:02:04 +0700

    Handle Blazor Step 8

--- LibraryManager.Blazor/Program.cs
+++ LibraryManager.Blazor/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using LibraryManager.Data;
+using LibraryManager.Services;
+using LibraryManager.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using LibraryManager.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +11,16 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<BookRepository>();
+builder.Services.AddScoped<MemberRepository>();
+builder.Services.AddScoped<BorrowRepository>();
+builder.Services.AddScoped<BookHistoryRepository>();
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<IBorrowService, BorrowService>();
+builder.Services.AddScoped<IBookHistoryService, BookHistoryService>();
 
 await builder.Build().RunAsync();
